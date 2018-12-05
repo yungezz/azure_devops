f1c911b7c12d0ddce4df986449f0f41fd2cf11b4
Yunge Zhu <yungez@microsoft.com>
2018-12-05 15:35:36 +0800

    customize for ansible

--- CalculateTemplateHash/listHashesST.ps1
+++ CalculateTemplateHash/listHashesST.ps1
@@ -16,17 +16,24 @@ param(
 )
 
 process {
-    $output = git --git-dir $QsRepoRoot log --name-only --format='%H' --follow -- $CurrentFolderName\jenkins\mainTemplate.json | Out-String
+    $output = git --git-dir $QsRepoRoot log --name-only --format='%H' --follow -- $CurrentFolderName\mainTemplate.json | Out-String
     $commits=($output -split '[\r\n]') | Where-Object {$_}
     $hashes=@()
 
     For ($i=0; $i -lt $commits.Count; $i=$i+2) {
         $commit=$commits[$i]
         $filepath=$commits[$i+1]
-        $folderName=$filepath.Substring(0, $filepath.IndexOf('/'))
+        Write-Host ("commit is: $commit")
+        Write-Host ("filepath is: $filepath")
+        $folderName = "."
+        if ($filepath.IndexOf('/') > -1)
+        {
+            $folderName=$filepath.Substring(0, $filepath.IndexOf('/'))
+        }
+        Write-Host ("CurrentFolderName: $CurrentFolderName")
         if ($folderName -eq $CurrentFolderName -Or ($OldFolderNames.Count -gt 0 -And @($OldFolderNames).Contains($folderName)))
         {
-            $fileurl = "https://raw.githubusercontent.com/Azure/azure-devops-utils/$commit/$filepath"
+            $fileurl = "https://raw.githubusercontent.com/Azure/ansible/$commit/$filepath"
             Write-Verbose ("Hashing url '$fileurl'...")
             $hash = & "HashExe\CalculateTemplateHash.exe" $fileurl
             Write-Verbose ("Hash=$hash")
